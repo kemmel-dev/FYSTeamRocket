48b0c4a08b7573475102c3a8b74592495037c49c
twabba <tobiasvdh@hotmail.com>
2019-09-24 12:23:27 +0200

    added VSCode support

---
+++ out/source/FYSTeamRocket.java
@@ -0,0 +1,31 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class FYSTeamRocket extends PApplet {
+
+public void setup()
+{
+    
+    background(0);
+}
+  public void settings() {  size(200, 200); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "FYSTeamRocket" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
